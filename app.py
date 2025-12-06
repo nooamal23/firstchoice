5a3254a5436df04e7b5425b9ce87c262d448edf7
ImenAouissi <imenneaouissii@gmail.com>
2025-12-06 09:27:41 +0100

    fix: change the port

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))   # Render gives a random PORT
+    app.run(host="0.0.0.0", port=port)
